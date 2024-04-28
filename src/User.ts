41fafa4d2bbfb0d08b5dcb086150b8dad15237b0
nazimgithub <nazim.mailtonazim@gmail.com>
2024-04-29 00:23:20 +0530

    first commit

---
+++ src/User.ts
@@ -0,0 +1,26 @@
+import { faker } from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
+
+export class User implements Mappable{
+  name: string;
+  location: {
+    lat: number;
+    lng: number
+  };
+  color: string = 'red';
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+      <h2>User Name is : ${this.name}</h2>
+    </div>`;  
+  }
+}
